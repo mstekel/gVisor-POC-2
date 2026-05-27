a0cecdeca76af7bef8faf084a7abb2236ba480f7
moshe.stekel <moshe.stekel@pyramidanalytics.com>
2026-05-27 11:54:55 +0300

    Network demo fixes

--- setup-netns.sh
+++ setup-netns.sh
@@ -1,20 +1,10 @@
 #!/bin/bash
-# setup-netns.sh <netns-name> <action>
-#
-# action=create:  create veth pair, configure namespace, add iptables rules
-# action=destroy: tear everything down cleanly
-#
-# After 'create', the sandbox should be launched with:
-#   --network=host (gVisor shares host netns)
-#   OCI config: { "type": "network", "path": "/var/run/netns/<netns-name>" }
-#
-# Host veth address:    10.0.0.1  (reachable from sandbox as the "host")
-# Sandbox veth address: 10.0.0.2
+# setup-netns.sh <netns-name> <create|destroy>
 
 NETNS=$1
 ACTION=$2
-VETH_HOST="veth-host-${NETNS}"
-VETH_SANDBOX="veth-sb-${NETNS}"
+VETH_HOST="veth0"
+VETH_SANDBOX="veth1"
 HOST_IP="10.0.0.1"
 SANDBOX_IP="10.0.0.2"
 ECHO_PORT=19876
@@ -31,7 +21,7 @@ create)
     ip netns add "$NETNS"
 
     echo "[netns] Creating veth pair: $VETH_HOST <-> $VETH_SANDBOX"
-    ip link add "$VETH_HOST"    type veth peer name "$VETH_SANDBOX"
+    ip link add "$VETH_HOST" type veth peer name "$VETH_SANDBOX"
 
     echo "[netns] Moving $VETH_SANDBOX into namespace $NETNS"
     ip link set "$VETH_SANDBOX" netns "$NETNS"
@@ -44,16 +34,17 @@ create)
     ip netns exec "$NETNS" ip addr add "${SANDBOX_IP}/24" dev "$VETH_SANDBOX"
     ip netns exec "$NETNS" ip link set "$VETH_SANDBOX" up
     ip netns exec "$NETNS" ip link set lo up
-    # Default route: all traffic from sandbox goes via host veth
     ip netns exec "$NETNS" ip route add default via "$HOST_IP"
 
     echo "[netns] Adding iptables rules"
     # Allow sandbox -> host echo server
-    iptables -I FORWARD -s "$SANDBOX_IP" -d "$HOST_IP" --dport "$ECHO_PORT" -p tcp -j ACCEPT
+    iptables -I FORWARD -s "$SANDBOX_IP" -d "$HOST_IP" -p tcp -m tcp --dport "$ECHO_PORT" -j ACCEPT
     # Allow established/related back
     iptables -I FORWARD -m state --state ESTABLISHED,RELATED -j ACCEPT
-    # Drop everything else from sandbox (blocks external internet)
+    # Drop everything else from sandbox
     iptables -I FORWARD -s "$SANDBOX_IP" ! -d "$HOST_IP" -j DROP
+    # Enable forwarding
+    echo 1 > /proc/sys/net/ipv4/ip_forward
 
     echo "[netns] Done. Sandbox can reach $HOST_IP:$ECHO_PORT only."
     echo "[netns] Namespace path: /var/run/netns/$NETNS"
@@ -61,7 +52,7 @@ create)
 
 destroy)
     echo "[netns] Removing iptables rules"
-    iptables -D FORWARD -s "$SANDBOX_IP" -d "$HOST_IP" --dport "$ECHO_PORT" -p tcp -j ACCEPT 2>/dev/null
+    iptables -D FORWARD -s "$SANDBOX_IP" -d "$HOST_IP" -p tcp -m tcp --dport "$ECHO_PORT" -j ACCEPT 2>/dev/null
     iptables -D FORWARD -m state --state ESTABLISHED,RELATED -j ACCEPT 2>/dev/null
     iptables -D FORWARD -s "$SANDBOX_IP" ! -d "$HOST_IP" -j DROP 2>/dev/null
 
